547ee3d3d0fc90e308ebb94782c8f8a3f9eba0f9
Jean Caio <111510390+jeancaiocs@users.noreply.github.com>
2026-02-10 12:31:57 -0300

    Fix assets path

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-frontend/',
-  build: {
-    assetsInlineLimit: 0
-  }
+  base: './'  
 })
